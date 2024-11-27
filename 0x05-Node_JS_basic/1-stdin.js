48f6953673fa9368ce246d56780be368c9c892ca
Bismark-K <kbismarkkwaku@gmail.com>
2024-11-27 09:16:05 +0000

    Solution to task 1

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,13 @@
+// A program that will be executed through command line
+// Author: Bismark-K
+
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  if (name !== null) {
+    process.stdout.write(`Your name is: ${name}`);
+  }
+});
+process.on('exit', () => {
+  process.stdout.write('This important software is now closing\n');
+});
